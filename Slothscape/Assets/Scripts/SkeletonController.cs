f213aa1b2999e1979908bacbd5d79942a77ed56e
Sam Barnes <barnsj2@student.op.ac.nz>
2021-03-18 15:56:01 +1300

    added Enemy AI, Animation and a home position

---
+++ Slothscape/Assets/Scripts/SkeletonController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonController : MonoBehaviour
+{
+    private Animator myAnim;
+    private Transform target;
+    public Transform homepos;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float maxrange;
+    [SerializeField]
+    private float minrange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+        target = FindObjectOfType<PlayerController>().transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= maxrange && Vector3.Distance(target.position, transform.position) >= minrange)
+        {
+            FollowPlayer();
+        }
+        else if (Vector3.Distance(target.position, transform.position) >= maxrange)
+        {
+            GoHome();
+        }
+       
+    }
+
+    public void FollowPlayer()
+    {
+        myAnim.SetBool("IsMoving", true);
+        myAnim.SetFloat("MoveX", (target.position.x - transform.position.x));
+        myAnim.SetFloat("MoveY", (target.position.y - transform.position.y));
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+
+    public void GoHome()
+    {
+        myAnim.SetFloat("MoveX", (homepos.position.x - transform.position.x));
+        myAnim.SetFloat("MoveY", (homepos.position.y - transform.position.y));
+        transform.position = Vector3.MoveTowards(transform.position, homepos.position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, homepos.position) == 0)
+        {
+            myAnim.SetBool("IsMoving", false);
+        }
+    }
+}
